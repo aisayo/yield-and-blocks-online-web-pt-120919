ff663eabe05279190fda8c150a90debb2e2b268c
Aysan Isayo <aisayo@icloud.com>
2019-12-12 23:51:27 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,14 @@
 def hello_t(names)
   
-  
-  i = 0 
-  
-  while i < names.length 
-    yield names[i]
-    i += 1 
-  end 
-  names
+  if block_given?
+    
+    i = 0 
+    
+    while i < names.length 
+      yield names[i]
+      i += 1 
+    end 
+    names
 end
 
 
